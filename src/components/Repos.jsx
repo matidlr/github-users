0f6fc8101c04af418eceae74f4b8ff896d6cfa5e
Matias <mati.delarosa1984@gmail.com>
2024-03-26 18:54:59 -0300

    first chart

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -1,11 +1,11 @@
-import { Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts'
+import { Pie3D, Column3D, Bar3D, Doughnut2D, ExampleChart } from './Charts'
 import Wrapper from '../assets/wrappers/Repos';
 import { useContext } from 'react';
 import { GithubContext } from '../context';
 const Repos = () => {
   const {repos} = useContext(GithubContext);
   return (
-    <div>Repos</div>
+    <ExampleChart />
   )
 }
 
