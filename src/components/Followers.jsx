f10c6a2b4fda85bf2bf08e4f2c18e24a45a0cb57
Matias <mati.delarosa1984@gmail.com>
2024-03-26 17:37:53 -0300

    followers

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -1,8 +1,32 @@
-
+import { useContext } from 'react';
+import Wrapper from '../assets/wrappers/Followers';
+import { GithubContext } from '../context';
 
 const Followers = () => {
+  const {followers} = useContext(GithubContext);
+
+  if(followers.length < 1) {
+    return <h2>No Followers Found</h2>
+  }
+  
   return (
-    <div>Followers</div>
+    <Wrapper>
+    <span className="label">Followers</span>
+    <div className="followers-div">
+      {followers.map((c) => {
+        const { avatar_url, login, html_url } = c
+        return (
+          <article key={c.id}>
+            <img className="follower-img" src={avatar_url} alt={login} />
+            <div className="content">
+              <h3 className="name">{login}</h3>
+              <p className="link">{html_url}</p>
+            </div>
+          </article>
+        )
+      })}
+    </div>
+  </Wrapper>
   )
 }
 
