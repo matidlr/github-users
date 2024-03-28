e10a44082000f3537578320585242fd14cd33436
Matias <mati.delarosa1984@gmail.com>
2024-03-28 12:25:27 -0300

    requests

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Wrapper from "../assets/wrappers/SearchBar";
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from "../context";
 
 
 const SearchBar = () => {
-    const [user,setUser] = useState('')
+    const [user,setUser] = useState('');
+    const {requests} = useContext(GithubContext);
 
     const handleSubmit = (e) =>{
       e.preventDefault();
+      if (user) {
+
+      }
     }
   return (
     <section className="section">
@@ -16,12 +20,17 @@ const SearchBar = () => {
             <form onSubmit={handleSubmit}>
                <div className="form-control">
                   <MdSearch />
-                  <input type='text' placeholder="enter hithub user"
-                  value={user} onChange={()=> setUser(e.target.value)} />
-                  <button type='submit'>search</button>
+                  <input 
+                      type='text' 
+                      placeholder="enter hithub user"
+                      value={user} 
+                      onChange={()=> setUser(e.target.value)} 
+                  />
+                  {requests > 0 &&  <button type='submit'>search</button>}
+                 
                </div>
             </form>
-            <h3>requests : 60 / 60</h3>
+            <h3>requests : {requests} / 60</h3>
         </Wrapper>
     </section>
   )
--- src/context.jsx
+++ src/context.jsx
@@ -12,8 +12,28 @@ const GithubProvider = ({children}) =>{
    const [githubUser, setGithubUser] = useState(mockUser);
    const [repos, setRepos] = useState(mockRepos);
    const [followers, setFollowers] = useState(mockFollowers);
+   // request loading
+   cont [requests, setRequests] = useState(0);
+   const [loading, setLoading] = useState(false);
+  // check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+    .then(({data}) =>{
+      let {rate:{remaining}} = data;
+      setRequests(remaining);
+      if (remaining === 0) {
+
+      }
+    })
+    .catch((err) => console.log(err));
+  };
+
+   // error
+   useEffect(checkRequests,[]);
   return (
-  <GithubContext.Provider value={{githubUser, repos, followers}}>{children}</GithubContext.Provider>
+  <GithubContext.Provider value={{githubUser, repos, followers, requests}}>
+  {children}
+  </GithubContext.Provider>
   );
 };
 
