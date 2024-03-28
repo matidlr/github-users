fc023e1d14886b10ae0401507dfb4525a7e891f4
Matias <mati.delarosa1984@gmail.com>
2024-03-28 18:46:45 -0300

    search user

--- src/context.jsx
+++ src/context.jsx
@@ -18,6 +18,16 @@ const GithubProvider = ({children}) =>{
 
    // error
    const [error, setError] = useState({show:false,msg:''})
+
+   const searchGithubUser = async (user) => {
+    const response = (await axios(`${rootUrl}/users/${user}`)).
+    catch(err => console.log(err))
+    if(response){
+      setGithubUser(response.data)
+    } else{
+      toggleError(true, 'there is no user with that user name')
+    }
+   }
   // check rate
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
@@ -32,13 +42,13 @@ const GithubProvider = ({children}) =>{
     .catch((err) => console.log(err));
   };
 
-  function toggleError(show,msg){
+  function toggleError(show = false,msg = ''){
       setError({show,msg})
   }
 
    useEffect(checkRequests,[]);
   return (
-  <GithubContext.Provider value={{githubUser, repos, followers, requests}}>
+  <GithubContext.Provider value={{githubUser, repos, followers, requests, error, searchGithubUser}}>
   {children}
   </GithubContext.Provider>
   );
