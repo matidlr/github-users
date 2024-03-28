0a75212db62f25c5a51805f1258474e3a0478666
Matias <mati.delarosa1984@gmail.com>
2024-03-28 11:32:49 -0300

    search component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState } from "react";
+import Wrapper from "../assets/wrappers/SearchBar";
+import { MdSearch } from 'react-icons/md';
+import { GithubContext } from "../context";
+
 
 const SearchBar = () => {
+    const [user,setUser] = useState('')
+
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+    }
   return (
-    <div>SearchBar</div>
+    <section className="section">
+        <Wrapper className="section-center">
+            <form onSubmit={handleSubmit}>
+               <div className="form-control">
+                  <MdSearch />
+                  <input type='text' placeholder="enter hithub user"
+                  value={user} onChange={()=> setUser(e.target.value)} />
+                  <button type='submit'>search</button>
+               </div>
+            </form>
+            <h3>requests : 60 / 60</h3>
+        </Wrapper>
+    </section>
   )
 }
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,13 +2,16 @@ import Card from '../components/Card';
 import Followers from '../components/Followers';
 import UserInfo from '../components/UserInfo';
 import Wrapper from '../assets/wrappers/Dashboard';
+import SearchBar from '../components/SearchBar';
+import Repos from '../components/Repos';
 
 const Dashboard = () => {
   return (
     <main className='section'>
       <Wrapper className='section-center'>
-          <Card></Card>
-          <Followers></Followers>
+          <SearchBar/>
+          <UserInfo/>
+          <Repos/>
       </Wrapper>
         
     </main>
